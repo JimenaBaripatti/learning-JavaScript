9ebeb58937f45750113dba7866d948c7a7abe0cf
Jimena Baripatti <jimenabaripatti@gmail.com>
2018-11-25 22:02:13 -0500

    Interactions and Conditional operators

---
+++ cond.js
@@ -0,0 +1,6 @@
+let company = prompt( `What is the “official” name of JavaScript?`,"")
+if (company == `ECMAScript`) {
+    alert('Right!');
+}
+    else { alert(`Didn’t know? ECMAScript!`);
+}
---
+++ interactions.js
@@ -0,0 +1,2 @@
+let age = prompt('How old are you?', 99)
+alert(`You are ${age} years old`);
